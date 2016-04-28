42bcc319227998a11084612dfba71545cbec5be7
kymtgt <github@tsugu.to>
2016-04-28 12:03:13 +0900

    [modify] background-positionを中央に

--- src/background.js
+++ src/background.js
@@ -1,6 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(){
   var url = localStorage['trelloBGUrl'] ? localStorage['trelloBGUrl'] : "http://www.earth1999.jp/wallpaper/3ughssda/wallpaper_pc.jpg";
   chrome.tabs.executeScript(null, {
-    "code": "document.getElementsByClassName('body-board-view')[0].style.backgroundImage = \"url("+url+")\""
+    "code": "document.getElementsByClassName('body-board-view')[0].style.backgroundImage = \"url("+url+")\";document.getElementsByClassName('body-board-view')[0].style.backgroundPosition='center center'"
   });
 });
